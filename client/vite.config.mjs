b87cfc974b4e016b898658e8c02198dc56264f96
HeritageRealtySolutions <marlon@heritagerealtysolutions.net>
2026-05-02 17:05:48 -0500

    Update vite.config.mjs

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -3,9 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.PNG', '**/*.png'],
+  assetsInclude: ['**/*.PNG', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      }
+    }
+  }
 })
